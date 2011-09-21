f3fd6f67d44ff6be0d0c02599aa5c468b683f2b1
linzhixiong <darlinglele@gmail.com>
2011-09-21 22:36:24 +0800

    arrayList<T>

--- console/src/Program.java
+++ console/src/Program.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+
 /**
  * Created by IntelliJ IDEA.
  * User: twer
@@ -8,5 +10,14 @@
 public class Program {
     public static void main(String[] args) {
         System.out.println("");
+
+        ArrayList<String> names;
+        names = new ArrayList<String>();
+        names.add("linzhixiong");
+        names.add("xuebo");
+        names.add("jinming");
+        for (String name : names) {
+            System.out.println(name);
+        }
     }
 }
