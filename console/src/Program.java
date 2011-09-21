aec69690623f0d2ed2e0608c1e7d0fcbc0692c03
linzhixiong <darlinglele@gmail.com>
2011-09-21 21:33:38 +0800

    init the project

---
+++ console/src/Program.java
@@ -0,0 +1,12 @@
+/**
+ * Created by IntelliJ IDEA.
+ * User: twer
+ * Date: 9/21/11
+ * Time: 9:24 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class Program {
+    public static void main(String[] args) {
+        System.out.println("");
+    }
+}
